c874a7691ef7762943c40dea246f03d1ecc52495
SnavarroM <chilote7613@gmail.com>
2023-10-03 10:33:01 -0300

    v2 uno_descarga_mail_xml.py

--- uno_descarga_mail_xml.py
+++ uno_descarga_mail_xml.py
@@ -2,13 +2,14 @@ import time
 import os
 import email
 import datetime
-from imbox import Imbox
+import imaplib
+import base64
 
 # Configuración de conexión IMAP
 host = "imap.gmail.com"
-username = "copiafactura@cenabast.cl"
-password = 'Cena@2023'
-download_folder = r'C:\Users\chilo\Downloads\ejecucion_xml-20230819T172426Z-001\ejecucion_xml\script xml\data'
+username = "pruebafactura@cenabast.cl"
+password = 'mbbv kxua vrsk wcdo'
+download_folder = r'C:\Users\snavarro\Desktop\proyecto\factura_xml-main_2\carga_produccion\data'
 
 # Verificar si la carpeta de destino existe, si no, crearla
 if not os.path.exists(download_folder):
@@ -38,25 +39,41 @@ while True:
             print(f"Error al eliminar los archivos: {str(e)}")
 
     # Conexión al servidor IMAP
-    with Imbox(host, username=username, password=password, ssl=True, ssl_context=None, starttls=False) as mail:
-        # Obtener todos los correos electrónicos del día actual
-        messages = mail.messages(date__on=datetime.date.today())
-        print("Obteniendo correos electrónicos del día...")
+    mail = imaplib.IMAP4_SSL(host)
+    mail.login(username, password)
 
-        # Recorrer todos los correos electrónicos
-        for uid, message in messages:
-            print(f"Procesando correo electrónico con UID: {uid}")
-            # Verificar si el correo electrónico tiene archivos adjuntos
-            if message.attachments:
-                print("Correo electrónico tiene archivos adjuntos...")
-                for attachment in message.attachments:
-                    print("Encontrado archivo adjunto...")
-                    # Obtener el nombre del archivo adjunto
-                    filename = attachment.get('filename')
-                    # Decodificar el nombre del archivo adjunto
+    # Seleccionar la bandeja de entrada
+    mail.select("inbox")
+
+    # Buscar todos los correos electrónicos no leídos
+    status, messages = mail.search(None, "UNSEEN")
+
+    # Recorrer todos los correos electrónicos
+    for num in messages[0].split():
+        print(f"Procesando correo electrónico con número: {num}")
+
+        # Obtener el correo electrónico
+        status, message = mail.fetch(num, "(RFC822)")
+
+        # Decodificar el correo electrónico
+        message = email.message_from_bytes(message[0][1])
+
+        # Verificar si el correo electrónico tiene archivos adjuntos
+        if message.get_content_maintype() == 'multipart':
+            for part in message.walk():
+                if part.get_content_maintype() == 'multipart':
+                    continue
+                if part.get('Content-Disposition') is None:
+                    continue
+
+                filename = part.get_filename()
+
+                # Decodificar el nombre del archivo adjunto
+                if filename:
                     decoded_filename = email.header.decode_header(filename)[0][0]
                     if isinstance(decoded_filename, bytes):
                         decoded_filename = decoded_filename.decode()
+
                     # Verificar si el archivo adjunto tiene extensión .xml o .XML
                     if decoded_filename.lower().endswith(('.xml', '.XML')):
                         # Generar el nombre único para el archivo XML
@@ -67,19 +84,25 @@ while True:
                         # Guardar el archivo adjunto en la carpeta de destino con el nombre único
                         filepath = os.path.join(download_folder, unique_name)
                         print(f"Guardando archivo adjunto en: {filepath}")
-
                         try:
                             with open(filepath, 'wb') as f:
-                                f.write(attachment.get('content').read())
+                                f.write(base64.b64decode(part.get_payload()))
                             print("Archivo adjunto guardado exitosamente.")
+
+                            # Marcar el correo electrónico como leído
+                            mail.store(num, '+FLAGS', '\\Seen')
                         except FileNotFoundError:
                             print("Error: No se pudo encontrar la carpeta de destino.")
                         except Exception as e:
                             print(f"Error al guardar el archivo adjunto: {str(e)}")
                     else:
                         print("El archivo adjunto no es un archivo XML. No se descargará.")
-            else:
-                print("El correo electrónico no tiene archivos adjuntos.")
+        else:
+            print("El correo electrónico no tiene archivos adjuntos.")
+
+    # Cerrar la conexión al servidor IMAP
+    mail.close()
+    mail.logout()
 
     # Esperar 60 segundos antes de repetir el proceso
     time.sleep(60)
